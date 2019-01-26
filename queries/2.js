660dbc122448224b7705b4a5983ef52a1f91bda0
mtorak <mtorak@dt140340005867a.intracomtel.com>
2019-01-26 13:28:36 +0200

    Update queries to work with ISODates

--- queries/2.js
+++ queries/2.js
@@ -2,8 +2,8 @@ db.incidents.aggregate([
    {
       "$match":{
          "creationDate":{
-            "$gt":"2011-01-01T00:00:00",
-            "$lt":"2018-01-03T00:00:00"
+            "$gt": ISODate("2011-01-01T00:00:00"),
+            "$lt": ISODate("2018-01-03T00:00:00")
          },
          "serviceType" : "Garbage Cart Black Maintenance/Replacement"
       }
