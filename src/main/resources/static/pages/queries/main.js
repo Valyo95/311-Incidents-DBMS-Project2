2bbf213a8db212d1ec2a38956f659b0aa119eeb2
mtorak <mtorak@dt140340005867a.intracomtel.com>
2019-01-28 01:02:46 +0200

    Run queries page done

--- src/main/resources/static/pages/queries/main.js
+++ src/main/resources/static/pages/queries/main.js
@@ -2,46 +2,71 @@
 
 appControllers.controller("QueriesController", function($scope, $http, $location, $rootScope, $window, notify) {
 		
-	$scope.searchPath = '/getTotalRequestsPerType';
-	$scope.searchResults = [];
-	$scope.hasSearched = false;
+	$scope.endpointPath = '/query/';
 
-	$scope.searchParams = {
-		startDate : "",
-		endDate : ""
-	};
+	$scope.startDate = "";
+	$scope.endDate = "";
+	$scope.type = "";
+	$scope.day = "";
+	$scope.name = "";
 
-	$scope.getIsLoggedIn = function () {
-        return $rootScope.isLoggedIn;
-  	};
+	$scope.lat1 = "";
+	$scope.lon1 = "";
+	$scope.lat2 = "";
+	$scope.lon2 = "";
+	
+	$scope.queryNumbers = [
+		"1",
+		"2",
+		"3",
+		"4",
+		"5",
+		"6",
+		"7",
+		"8",
+		"9",
+		"10",
+		"11"
+	];
+	$scope.queryNumber = $scope.queryNumbers[0];
 
-	$scope.search = function() {
-		$scope.hasSearched = true;
+	$scope.runQuery = function() {
+		var url = $scope.endpointPath + $scope.queryNumber;
 
-		$http({
-			method : "GET",
-			url : $scope.searchPath,
-			params : {
-				fromDate: $scope.searchParams.startDate,
-				toDate: $scope.searchParams.endDate
-			},
-			headers : {
-			  'Content-Type' : 'application/json'
-			}
-		})
-		.then(
-			function(response) {
-				$scope.searchResults = response.data;
-			},
-			_error)
+		switch ($scope.queryNumber) {
+			case ("1"):
+				url = url + "?start=" + $scope.startDate + "&end=" + $scope.endDate;
+				break;
+			case ("2"):
+				url = url + "?start=" + $scope.startDate + "&end=" + $scope.endDate
+					  + "&type=" + $scope.type;
+				break;
+			case ("3"):
+				url = url + "?day=" + $scope.day;
+				break;
+			case ("4"):
+				url = url + "?type=" + $scope.type;
+				break;
+			case ("5"):
+				url = url + "?start=" + $scope.startDate + "&end=" + $scope.endDate;
+				break;
+			case ("6"):
+				url = url + "?x1=" + $scope.lat1 + "&y1=" + $scope.lon1
+					  + "&x2=" + $scope.lat2 + "&y2=" + $scope.lon2
+					  + "&day=" + $scope.day;
+				break;
+			case ("7"):
+				url = url + "?day=" + $scope.day;
+				break;
+			case ("11"):
+				url = url + "?name=" + $scope.name;
+				break;
+			default:
+				break;
+		}
+
+		$window.open(url, '_blank');
 	}
-
-   function _error(response) {
-  		notify({message: response, duration: 5000});
-   		console.log(response);
-   }
-
-
 
 });
 		
