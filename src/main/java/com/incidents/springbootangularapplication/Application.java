a1ad7da59e1a1a02d1843821d3adedc8a01413d0
mtorak <mtorak@dt140340005867a.intracomtel.com>
2019-01-27 20:55:21 +0200

    Add missing annotations for spring boot scanning

--- src/main/java/com/incidents/springbootangularapplication/Application.java
+++ src/main/java/com/incidents/springbootangularapplication/Application.java
@@ -1,9 +1,17 @@
 package com.incidents.springbootangularapplication;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.incidents")
+@EnableAutoConfiguration
+@EntityScan("com.incidents")
+@EnableMongoRepositories("com.incidents")
+@ComponentScan("com.incidents")
 public class Application {
 
 	public static void main(String[] args) {
