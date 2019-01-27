742f1925d2f0537c2ea1d3e18083d0ffd82aea22
Valyo95 <Valyo95@abv.bg>
2019-01-27 23:51:44 +0200

    6 query ready

--- src/main/java/com/incidents/web/QueriesEndpoint.java
+++ src/main/java/com/incidents/web/QueriesEndpoint.java
@@ -11,43 +11,43 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.incidents.services.impl.QueriesImpl;
 
-
-
 @RestController
 @RequestMapping("query")
 public class QueriesEndpoint {
 
 	@Autowired
 	private QueriesImpl service;
-		
-	
+
 	@RequestMapping(value = "/1", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
-	public List<Object> first(@RequestParam("start") String start, @RequestParam("end") String end) throws ParseException{
+	public List<Object> first(@RequestParam("start") String start, @RequestParam("end") String end)
+			throws ParseException {
 		return service.first(start, end);
 	}
-	
+
 	@RequestMapping(value = "/2", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
-	public List<Object> second(@RequestParam("type") String type, @RequestParam("start") String start, @RequestParam("end") String end) throws ParseException{
+	public List<Object> second(@RequestParam("type") String type, @RequestParam("start") String start,
+			@RequestParam("end") String end) throws ParseException {
 		return service.second(type, start, end);
 	}
-	
+
 	@RequestMapping(value = "/3", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
-	public List<Object> second(@RequestParam("day") String day) throws ParseException{
+	public List<Object> second(@RequestParam("day") String day) throws ParseException {
 		return service.third(day);
 	}
-	
+
 	@RequestMapping(value = "/4", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
-	public List<Object> forth(@RequestParam("type") String type) throws ParseException{
+	public List<Object> forth(@RequestParam("type") String type) throws ParseException {
 		return service.forth(type);
 	}
-	
+
 	@RequestMapping(value = "/5", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
-	public List<Object> fifth(@RequestParam("start") String start, @RequestParam("end") String end) throws ParseException{
+	public List<Object> fifth(@RequestParam("start") String start, @RequestParam("end") String end)
+			throws ParseException {
 		return service.fifth(start, end);
 	}
-	
+
 	@RequestMapping(value = "/7", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
-	public List<Object> seventh(@RequestParam("day") String day){
+	public List<Object> seventh(@RequestParam("day") String day) {
 		try {
 			return service.seventh(day);
 		} catch (ParseException e) {
@@ -55,25 +55,31 @@ public class QueriesEndpoint {
 			e.printStackTrace();
 		}
 		return null;
-	}	
-	
+	}
+
 	@RequestMapping(value = "/8", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
-	public List<Object> eight() throws ParseException{
+	public List<Object> eight() throws ParseException {
 		return service.eight();
 	}
-	
+
 	@RequestMapping(value = "/9", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
-	public List<Object> nine() throws ParseException{
+	public List<Object> nine() throws ParseException {
 		return service.nine();
 	}
-	
+
 	@RequestMapping(value = "/10", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
-	public List<Object> ten() throws ParseException{
+	public List<Object> ten() throws ParseException {
 		return service.ten();
 	}
-	
+
 	@RequestMapping(value = "/11", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
-	public List<Object> eleven(@RequestParam("name") String name) throws ParseException{
+	public List<Object> eleven(@RequestParam("name") String name) throws ParseException {
 		return service.eleven(name);
 	}
+
+	@RequestMapping(value = "/6", method = RequestMethod.GET, headers = "Accept=application/json", produces = "application/json")
+	public List<Object> six(@RequestParam("day") String day, @RequestParam("x1") Double x1, @RequestParam("y1") Double y1, @RequestParam("x2") Double x2, @RequestParam("y2") Double y2) throws ParseException {
+		return service.six(day, x1, y1, x2, y2);
+	}
+
 }
