8e71845da16a1aea1a14e4507d486f58695053d5
mtorak <mtorak@dt140340005867a.intracomtel.com>
2019-01-27 23:03:13 +0200

    New alternative for query 6 (needs location transform script)

--- queries/6.js
+++ queries/6.js
@@ -1,3 +1,49 @@
+// Works after running the convertLocationScript
+db.incidents.aggregate([
+  {
+       "$match" : {
+         "creationDate": ISODate("2011-01-01T00:00:00.000Z"),
+         "location.coordinates": {
+          "$geoWithin": {
+            "$box": [
+                [
+                    40.99825021043669,
+                    -80.543586730957031
+                ],
+                [
+                    60.139285461134996,
+                    -100.996772766113281
+                ]
+             ]
+          }
+        }
+      }
+   },
+   {
+      "$group": {
+         "_id": "$serviceType",
+         "totalRequestsPerType": {
+            "$sum": 1   
+         }
+      }
+   },
+   {
+      "$sort": {
+        "totalRequestsPerType": -1   
+      }
+   },
+   {
+      "$limit": 1   
+   },
+   {
+      "$project": {
+        "_id": 1/*,
+        "totalRequestsPerType": 0*/
+      }   
+   }
+])
+
+//Runs ok directly but hard to write in Java
 db.incidents.aggregate([
    {
       "$project":{
